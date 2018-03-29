41d6ccf74d99d9c0bc83a5c2243cfd08b220918d
youngju <asdadssad@naver.coim>
2018-03-29 16:04:01 +0900

    소수

--- primeNumberCheck/ViewController.swift
+++ primeNumberCheck/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var txtF: UITextField!
+    @IBOutlet weak var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtF.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnR(_ sender: Any) {
+        txtF.text = ""
+        lbl.text = ""
+    }
+    
+    @IBAction func btn(_ sender: Any) {
+            let number = Int(txtF.text!)
+            var isPrise = true
+            if number! == 1 {
+                isPrise = false
+   
+        }
+        if number! != 1 && number! != 2 {
+            for i in 2 ..< number!  {
+                if number! % i == 0  {
+                    isPrise = false
+                }
+        }
+}
+        if isPrise == true  {
+            lbl.text = "Prime number"
+        }else {
+            lbl.text = "Not prime number"
+        }
+}
+func textFieldShouldReturn(_ textField: UITextField) -> Bool  {txtF.resignFirstResponder()
+return true
+}
 }
 
